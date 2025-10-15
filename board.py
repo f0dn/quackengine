865716dffac3754eafbf8973ee11f4f76b4c0a53
Malik Delana Taiwo <maliktadelana@gmail.com>
2025-10-15 19:08:18 -0400

    gets all possible bishop moves for both white and black pieces

--- board.py
+++ board.py
@@ -74,26 +74,32 @@ class Board:
 
         for i in range(8):
             for j in range(8):
-                if self.turn == 'w':
-                    # record all possible bishop moves
-                    if chess_board[i][j] == 'B':
-                        # keep track of starting position
-                        position = (i, j)
-                        
-                        directions = [(-1, -1), (-1,1), (1,1), (1,-1)]
-                        for direction in directions:
-                            new_point = (position[0] + direction[0], position[1] + direction[1])                      
-                            while 0 <= new_point[0] <= 7 and 0 <= new_point[1] <= 7:                             
-                                # Check if an opponent or teammate is on the diagonal path
+                # record all possible bishop moves
+                if chess_board[i][j] == 'B' or chess_board[i][j] == 'b':
+                    # keep track of starting position
+                    position = (i, j)
+
+                    directions = [(-1, -1), (-1,1), (1,1), (1,-1)]
+                    for direction in directions:
+                        new_point = (position[0] + direction[0], position[1] + direction[1])                      
+                        while 0 <= new_point[0] <= 7 and 0 <= new_point[1] <= 7:   
+                            if self.turn == 'w':                        
+                                # Check if an opponent or teammate is on the diagonal path                        
                                 if chess_board[new_point[0]][new_point[1]] != None and chess_board[new_point[0]][new_point[1]].islower():
                                     possible_moves.append(f"{chr(1+j+96)}{i+1}{chr(new_point[1]+1+96)}{new_point[0]+1}")
                                     break
-                                
+                                    
                                 elif chess_board[new_point[0]][new_point[1]] != None and chess_board[new_point[0]][new_point[1]].isupper():
                                     break
+                            else:
+                                # Check if an opponent or teammate is on the diagonal path                        
+                                if chess_board[new_point[0]][new_point[1]] != None and chess_board[new_point[0]][new_point[1]].isupper():
+                                    possible_moves.append(f"{chr(1+j+96)}{i+1}{chr(new_point[1]+1+96)}{new_point[0]+1}")
+                                    break
+                                    
+                                elif chess_board[new_point[0]][new_point[1]] != None and chess_board[new_point[0]][new_point[1]].islower():
+                                    break
 
-                                possible_moves.append(f"{chr(1+j+96)}{i+1}{chr(new_point[1]+1+96)}{new_point[0]+1}")
-                                new_point = (new_point[0] + direction[0], new_point[1] + direction[1])
-                else:
-                    pass
+                            possible_moves.append(f"{chr(1+j+96)}{i+1}{chr(new_point[1]+1+96)}{new_point[0]+1}")
+                            new_point = (new_point[0] + direction[0], new_point[1] + direction[1])
         return possible_moves

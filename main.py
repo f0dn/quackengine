ce255f7645a3e4eccb526e5959e0894cab576762
gigabyts <jaden.pradeep2007@gmail.com>
2025-10-27 10:25:51 -0400

    Added tuple for move storage, redid move parsing logic, added promotion moves properly being expressed

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
+from move import Move
+
 def main():
-    print("Hello World")
-  
-    
-        
-    
+    moves = []
+
+    while True:
+        user_input = input("Enter a move (or 'quit' to exit): ")
+
+        if user_input.lower() == "quit":
+            break
+        try:
+            move = Move(user_input)
+            moves.append(move.to_long_algebraic())
+            print(f"Move accepted: {move.to_long_algebraic()}")
+            print(f"Source coords: {move.src_coords}, Target coords: {move.target_coords}")
+        except Exception as e:
+            print(f"Invalid move: {e}")
+
+    move_history = tuple(moves)
+    print(move_history)
+
 
 if __name__ == "__main__":
     main()
-    
-        
-       
--- move.py
+++ move.py
@@ -1,15 +1,33 @@
 class Move:
-    def __init__(self, algebraic_notation):
-        move_clean = algebraic_notation.replace("x", "")
-        self.chess_piece = move_clean[0]
-        self.src_square = move_clean[1:3]
-        self.target_square = move_clean[-2:]
+    def __init__(self, move: str):
+        self.is_capture = 'x' in move
+        self.promoted_to = None
+        self.chess_piece = 'P'
 
-    def promotion_check(self):
-        is_promoted = False
-        if self.chess_piece == 'p' and self.target_square[1] in ['1', '8']:
-            is_promoted = True
-        return is_promoted
+        move = move.replace('-', '')
 
-    def san_notation(self):
-        return self.src_square + self.target_square
+        if move.startswith('p'):
+            move = move[1:]
+            self.chess_piece = 'P'
+
+        if len(move) > 4 and move[-1].isalpha() and move[-1].islower():
+            self.promoted_to = move[-1]
+            move = move[:-1]
+
+        move = move.replace('x', '')
+
+        if move and move[0].isupper() and move[0] in "KQRBN":
+            self.chess_piece = move[0]
+            move = move[1:]
+
+        #(file, rank)
+        self.src_coords = (ord(move[0]) - ord('a'), int(move[1]) - 1)
+        self.target_coords = (ord(move[2]) - ord('a'), int(move[3]) - 1)
+
+    def to_long_algebraic(self):
+        src = chr(self.src_coords[0] + ord('a')) + str(self.src_coords[1] + 1)
+        target = chr(self.target_coords[0] + ord('a')) + str(self.target_coords[1] + 1)
+        result = src + target
+        if self.promoted_to:
+            result += self.promoted_to
+        return result

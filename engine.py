d6851a3dd29941950de071841b220b96032b0109
Steven Lou <slou50@stuy.edu>
2026-03-07 17:09:41 -0500

    Resolved comments

--- engine.py
+++ engine.py
@@ -89,43 +89,23 @@ class Engine:
             cp, pv = self.minimax(self.board, depth)
             pv_str = " ".join(move.to_long_algebraic() for move in pv)
 
-            if (cp == float('-inf') or cp == float('inf')) :
-                print(f"info score cp {cp} depth {depth} nodes {nodes} time {time_ms} pv {pv_str}", flush=True)
-            else:
-                print(f"info score cp {int(cp)} depth {depth} nodes {nodes} time {time_ms} pv {pv_str}", flush=True)
+            if cp == float('-inf'):
+                cp = -100000000
+            else if cp == float('inf'):
+                cp = 100000000
+            
+            print(f"info score cp {int(cp)} depth {depth} nodes {nodes} time {time_ms} pv {pv_str}", flush=True)
 
             best_move = pv[0]
             print("bestmove " + best_move.to_long_algebraic(),  flush=True)
 
-                # if not self.searching:
-                #     self.searching = True
-                    
-                #     def info():
-                #         while self.searching:
-                #             moves = self.board.get_possible_moves()
-                #             print(moves)
-                #             time.sleep(0.5)
-
-                #     self.search_thread = threading.Thread(target = info)
-                #     self.search_thread.start()
         elif(command == "stop"):
-            # self.searching = False
-            
-            # if self.search_thread:
-            #     self.search_thread.join()
-            #     self.search_thread = None
-
-            # print(self.calculate_best_move())
             pass
         elif(command == "ponderhit"):
             #ponder is when engine calculates opponent's next move during opponent's turn
             #normal search is when engine calculates its own move during its own turn
             #when user plays expected move, then engine should continue searching but switch from pondering to normal search
             pass
-
-    def calculate_best_move(self):
-        #just best move or also info?
-        pass
 
     def format_info(self, list_of_tuples):
         full_info_str = "info "

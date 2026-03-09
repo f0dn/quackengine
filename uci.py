17e26acbfac94ddf25b77dd5295dcc60ecddfe7d
AngelChen914 <angelch91406@gmail.com>
2026-03-09 13:38:12 -0400

    finished gocommand

--- uci.py
+++ uci.py
@@ -36,7 +36,9 @@ class PositionCommand(Command):
         return PositionCommand(startpos, fen, moves)
         
 class GoCommand(Command):
-    def __init__(self, depth: Optional[int] = None, movetime: Optional[int] = None, wtime: Optional[int] = None, btime: Optional[int] = None, winc: Optional[int] = None, binc: Optional[int] = None, infinite: bool = False):
+    def __init__(self, depth: Optional[int] = None, movetime: Optional[int] = None, wtime: Optional[int] = None, btime: Optional[int] = None, 
+                 winc: Optional[int] = None, binc: Optional[int] = None, infinite: bool = False, searchmoves: Optional[List[str]] = None, 
+                 ponder: bool = False, movestogo: Optional[int] = None, nodes: Optional[int] = None, mate: Optional[int] = None):
         self.depth = depth
         self.movetime = movetime
         self.wtime = wtime
@@ -44,11 +46,92 @@ class GoCommand(Command):
         self.winc = winc
         self.binc = binc
         self.infinite = infinite
+        self.searchmoves = searchmoves
+        self.ponder = ponder
+        self.movestogo = movestogo
+        self.nodes = nodes
+        self.mate = mate
 
     @staticmethod
     def parse(command_str: str) -> "GoCommand":
         tokens = command_str.strip().split()
         tokens.pop(0)
+        kwargs = {
+            "depth": None,
+            "movetime": None,
+            "wtime": None,
+            "btime": None,
+            "winc": None,
+            "binc": None,
+            "infinite": False,
+            "searchmoves": None,
+            "ponder": False,
+            "movestogo": None,
+            "nodes": None,
+            "mate": None,
+        }
+
+        keywords = ["depth", "movetime", "wtime", "btime", "winc", "binc", "infinite", "searchmoves", "ponder", "movestogo", "nodes", "mate"]
+
+        i = 0
+        while i < len(tokens):
+            token = tokens[i]
+
+            if token == "depth":
+                kwargs["depth"] = int(tokens[i + 1])
+                i += 2
+
+            elif token == "movetime":
+                kwargs["movetime"] = int(tokens[i + 1])
+                i += 2
+
+            elif token == "wtime":
+                kwargs["wtime"] = int(tokens[i + 1])
+                i += 2
+
+            elif token == "btime":
+                kwargs["btime"] = int(tokens[i + 1])
+                i += 2
+
+            elif token == "winc":
+                kwargs["winc"] = int(tokens[i + 1])
+                i += 2
+
+            elif token == "binc":
+                kwargs["binc"] = int(tokens[i + 1])
+                i += 2
+
+            elif token == "infinite":
+                kwargs["infinite"] = True
+                i += 1
+
+            elif token == "searchmoves":
+                i += 1
+                searchmoves_list = []
+                while i < len(tokens) and tokens[i] not in keywords:
+                    searchmoves_list.append(tokens[i])
+                    i += 1
+                kwargs["searchmoves"] = searchmoves_list
+
+            elif token == "ponder":
+                kwargs["ponder"] = True
+                i += 1
+
+            elif token == "movestogo":
+                kwargs["movestogo"] = (int)(tokens[i + 1])
+                i += 2
+
+            elif token == "nodes":
+                kwargs["nodes"] = (int)(tokens[i + 1])
+                i += 2
+
+            elif token == "mate":
+                kwargs["mate"] = (int)(tokens[i + 1])
+                i += 2
+            else:
+                i += 1
+
+        return GoCommand(**kwargs)
 
 def parse_command(command_str: str) -> Command:
     if not command_str:

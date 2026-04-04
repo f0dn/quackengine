442daff24e2aa0da6b8dea9efc4e0e23d16095bc
AngelChen914 <angelch91406@gmail.com>
2026-04-04 17:45:22 -0400

    added tests for parse_command

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,6 +1,7 @@
 from engine import Engine
-from board import Board
+from uci import parse_command, PositionCommand, GoCommand, UCICommand, IsReadyCommand, QuitCommand, SetOptionCommand, UCINewGameCommand, StopCommand
 
+#testing engine handle_input()
 def test_isready(capsys):
     engine = Engine()
     engine.handle_input("isready")
@@ -25,3 +26,48 @@ def test_setoption():
     engine = Engine()
     engine.handle_input("setoption name Hash value 64")
     assert engine.options_dict["Hash"] == "64"
+
+#testing uci parse_command()
+def test_position_startpos():
+    cmd = parse_command("position startpos")
+    assert isinstance(cmd, PositionCommand)
+    assert cmd.startpos is True
+    assert cmd.fen is None
+    assert cmd.moves == []
+
+def test_position_startpos_with_moves():
+    cmd = parse_command("position startpos moves e2e4 e7e5")
+    assert cmd.startpos is True
+    assert cmd.moves is not None
+    assert len(cmd.moves) == 2
+
+def test_go_depth():
+    cmd = parse_command("go depth 10")
+    assert isinstance(cmd, GoCommand)
+    assert cmd.depth == 10
+
+def test_uci_command():
+    cmd = parse_command("uci")
+    assert isinstance(cmd, UCICommand)
+
+def test_isready_command():
+    cmd = parse_command("isready")
+    assert isinstance(cmd, IsReadyCommand)
+
+def test_stop_command():
+    cmd = parse_command("stop")
+    assert isinstance(cmd, StopCommand)
+
+def test_quit_command():
+    cmd = parse_command("quit")
+    assert isinstance(cmd, QuitCommand)
+
+def test_ucinewgame_command():
+    cmd = parse_command("ucinewgame")
+    assert isinstance(cmd, UCINewGameCommand)
+
+def test_setoption_name_value():
+    cmd = parse_command("setoption name Hash value 128")
+    assert isinstance(cmd, SetOptionCommand)
+    assert cmd.name == "Hash"
+    assert cmd.value == "128"
